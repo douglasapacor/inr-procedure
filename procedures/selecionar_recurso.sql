e9c8b58bc01e91763dfc04338b91e4c31c56462c
Douglas Pacor <douglas@epicquestti.com.br>
2025-03-27 23:03:53 -0300

    doing

--- procedures/selecionar_recurso.sql
+++ procedures/selecionar_recurso.sql
@@ -15,14 +15,20 @@ BEGIN
         r.acoes,
         r.ativo,
         r.plataforma_id,
-        p.nome AS plataforma_nome,
-        pf.nome
+        pl.nome AS 'plataforma_nome',
+        pf.nome AS 'criado_nome',
+        r.criado_em,
+        pfi.nome AS 'alterado_nome',
+        r.alterado_em
     FROM 
         recurso r
+    INNER JOIN plataforma pl 
+        ON pl.id = r.plataforma_id
+    INNER JOIN perfil pf
+        ON pf.usuario_id = r.criado_id
+    INNER JOIN perfil pfi
+        ON pfi.usuario_id = r.alterado_id        
     WHERE r.id = recursoId
-    AND r.plataforma_id = p.id
-    AND r.criado_id = ui.id
-    AND pf.usuario_id = ui.id
     AND excluido_id IS NULL
     AND excluido_em is NULL;
 END;
